744bef3619b05a440cff1beb2a66c23eebd85a36
raihan-g-i-t <opu.raihankhan@gmail.com>
2025-11-13 16:38:17 +0600

    sting methods

--- String/methods.js
+++ String/methods.js
@@ -4,3 +4,20 @@ console.log("Raihan name length: " + userName.length);
 console.log(userName.toUpperCase());
 console.log(userName.toLowerCase());
 
+console.log(userName.slice(2,6));
+console.log(userName.replace("Rai", "Rey"));
+
+let userName2 = "Raihan";
+let temp = "";
+
+for(let key in userName2){
+    temp = temp.concat(userName2[key])
+}
+console.log(temp);
+
+temp = "";
+
+for(let i=0; i<userName2.length; i++){
+    temp = temp.concat(userName[i]);
+}
+console.log(temp);
