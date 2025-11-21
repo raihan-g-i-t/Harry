ab8e2f4e1e3c3ce78e252b6eec4fb9156981fd8d
raihan-g-i-t <opu.raihankhan@gmail.com>
2025-11-21 18:44:20 +0600

    console

---
+++ console/script.js
@@ -0,0 +1,9 @@
+alert("Enter a");
+let a = prompt("Enter a: ");
+document.writeln(a);
+
+//practice question
+
+let color = prompt("Enter the Background color");
+
+document.body.style.backgroundColor = color;
